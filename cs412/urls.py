296df3c0145add0a56061decbd517655550d2e27
Luisa Vazquez Usabiaga <lvu@bu.edu>
2025-09-12 15:35:15 -0400

    Add healthcheck endpoint

--- cs412/urls.py
+++ cs412/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf.urls.static import static
 from django.conf import settings 
+from django.http import HttpResponse
+def healthcheck(request):
+    return HttpResponse("Django is alive!")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("quotes.urls")),
     path("lvu/", include("quotes.urls")),
+    path("health/", healthcheck), 
 ]   +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
