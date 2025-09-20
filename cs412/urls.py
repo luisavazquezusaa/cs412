6a694b3bc412595f01addc528bf5d003e8a62ea1
Luisa Vazquez Usabiaga <lvu@bu.edu>
2025-09-19 20:01:57 -0400

    Added all files for deployment for restaurant app.

--- cs412/urls.py
+++ cs412/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("quotes.urls")),   
+    path("quotes/", include("quotes.urls")), 
+    path("restaurant/", include("restaurant.urls")), 
+
 ] + static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
